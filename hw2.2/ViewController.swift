09dad7518933c239c78488bf0e28c7f2c4ee669c
est54 <smirnyagin.semen@gmail.com>
2020-08-23 18:48:39 +0700

    Initial Commit

---
+++ hw2.2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hw2.2
+//
+//  Created by Семен on 23.08.2020.
+//  Copyright © 2020 Семен. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
