73ec15b67ce957f518588d6776f5857b77aa3ddc
est54 <smirnyagin.semen@gmail.com>
2020-08-23 21:09:17 +0700

    finish homework

--- hw2.2/ViewController.swift
+++ hw2.2/ViewController.swift
@@ -10,11 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainView: UIView!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redSlider.value = 0.0
+        greenSlider.value = 0.0
+        blueSlider.value = 0.0
+        
+        redLabel.text = "0.0"
+        greenLabel.text = "0.0"
+        blueLabel.text = "0.0"
+        
+        changeColorView(redValue: redSlider.value,
+                    greenValue: greenSlider.value,
+                    blueValue: blueSlider.value)
+        
     }
 
-
+    @IBAction func changeSliderValue(_ sender: Any) {
+        redLabel.text = String(format: "%.2f",redSlider.value)
+        greenLabel.text = String(format: "%.2f",greenSlider.value)
+        blueLabel.text = String(format: "%.2f",blueSlider.value)
+        changeColorView(redValue: redSlider.value,
+                    greenValue: greenSlider.value,
+                    blueValue: blueSlider.value)
+    }
+    
+    func changeColorView(redValue: Float, greenValue: Float, blueValue: Float) {
+        mainView.backgroundColor = .init(red: CGFloat(redValue),
+                                         green: CGFloat(greenValue),
+                                         blue: CGFloat(blueValue),
+                                         alpha: 1.0)
+    }
+    
 }
 
